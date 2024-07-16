436af447726664671f82394d081ec978db0e82a1
bay <bayeldzhoroev@gmail.com>
2024-07-16 23:14:33 +0300

    Add main logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import parseFile from './parser.js';
+import findDiff from './find-diff.js';
+import formatDifference from './formatter/formatter.js';
 
-// 1. receive values from the first file
-// 2. receive values form the second file
-// 3. gen differences between files
-// 4. return the result 
+const getFileContent = (path) => readFileSync(resolve(cwd(), path));
+const getFormat = (path) => path.split('.').at(-1);
+
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
+  const file1Parsed = parseFile(getFormat(filepath1), getFileContent(filepath1));
+  const file2Parsed = parseFile(getFormat(filepath2), getFileContent(filepath2));
+
+  const difference = findDiff(file1Parsed, file2Parsed);
+  const formattedResult = 
+  // formatters
+};
+
+export default genDiff;
