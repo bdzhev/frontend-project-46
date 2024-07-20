995ebc349dc17d73748e179e8579bffb6a70b9a7
bay <bayeldzhoroev@gmail.com>
2024-07-20 14:50:42 +0300

    Fix plain line constructor to be immutable

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -17,18 +17,22 @@ const makePlain = (difference, keyPrefix = '') => {
     switch (node.status) {
       case 'same':
         return acc;
-      case 'changed':
-        acc.push(`Property '${keyPrefix.concat(node.key)}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`);
-        return acc;
-      case 'removed':
-        acc.push(`Property '${keyPrefix.concat(node.key)}' was removed`);
-        return acc;
-      case 'added':
-        acc.push(`Property '${keyPrefix.concat(node.key)}' was added with value: ${formatValue(node.value)}`);
-        return acc;
-      case 'hasChildren':
-        acc.push(makePlain(node.value, keyPrefix.concat(node.key, '.')));
-        return acc;
+      case 'changed': {
+        const line = (`Property '${keyPrefix.concat(node.key)}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`);
+        return ([...acc, line]);
+      }
+      case 'removed': {
+        const line = `Property '${keyPrefix.concat(node.key)}' was removed`;
+        return ([...acc, line]);
+      }
+      case 'added': {
+        const line = `Property '${keyPrefix.concat(node.key)}' was added with value: ${formatValue(node.value)}`;
+        return ([...acc, line]);
+      }
+      case 'hasChildren': {
+        const line = makePlain(node.value, keyPrefix.concat(node.key, '.'));
+        return ([...acc, line]);
+      }
       default:
         throw new Error(`Unknown item status. ${node.status}`);
     }
