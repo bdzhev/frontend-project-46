566ddb379b4485deb8984376b0f12ad5ebdff8b0
bay <bayeldzhoroev@gmail.com>
2024-07-24 11:46:18 +0300

    Update test result names

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,28 +9,28 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
 const readFixture = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8').trim();
 
-const expectedResult1 = readFixture('resultStylish.txt');
-const expectedResult2 = readFixture('resultPlain.txt');
-const expectedResult3 = JSON.parse(readFixture('resultJSON.json'));
+const expectedStylishResult = readFixture('resultStylish.txt');
+const expectedPlainResult = readFixture('resultPlain.txt');
+const expectedJSONResult = JSON.parse(readFixture('resultJSON.json'));
 
 describe('genDiff functionality test', () => {
   test('JSON cases', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
 
-    expect(genDiff(filepath1, filepath2)).toEqual(expectedResult1);
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedResult1);
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedResult2);
-    expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedResult3);
+    expect(genDiff(filepath1, filepath2)).toEqual(expectedStylishResult);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylishResult);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlainResult);
+    expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedJSONResult);
   });
 
   test('YML and YAML cases', () => {
     const filepath1 = getFixturePath('file1.yml');
     const filepath2 = getFixturePath('file2.yaml');
 
-    expect(genDiff(filepath1, filepath2)).toEqual(expectedResult1);
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedResult1);
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedResult2);
-    expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedResult3);
+    expect(genDiff(filepath1, filepath2)).toEqual(expectedStylishResult);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylishResult);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlainResult);
+    expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedJSONResult);
   });
 });
