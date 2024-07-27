39ffea35a4a468d5d026d9e260506f29d69d4ae8
bay <bayeldzhoroev@gmail.com>
2024-07-27 15:48:58 +0300

    Refactor data parsing function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import { readFileSync } from 'fs';
 import { resolve, extname } from 'path';
 import { cwd } from 'process';
@@ -6,12 +5,15 @@ import parse from './parse.js';
 import findDiff from './find-diff.js';
 import makeFormatted from './formatter/formatter.js';
 
-const getContent = (path) => readFileSync(resolve(cwd(), path));
-const getFileType = (path) => extname(path).slice(1);
+const getData = (filepath) => {
+  const content = readFileSync(resolve(cwd(), filepath));
+  const type = extname(filepath).slice(1);
+  return parse(content, type);
+};
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const data1 = parse(getContent(filepath1), getFileType(filepath1));
-  const data2 = parse(getContent(filepath2), getFileType(filepath2));
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
 
   const difference = findDiff(data1, data2);
   const formattedResult = makeFormatted(difference, format);
